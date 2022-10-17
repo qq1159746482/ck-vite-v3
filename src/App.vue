f2ea83a84ceaae3638c41375ce91f06bc187e26b
PCOS-2022SXGVAQ\Administrator <1159746482@qq.com>
2022-10-17 17:08:54 +0800

    第三次更改文件提交

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { Ref, ref } from 'vue';
+
+let a:Ref<string> = ref('')
+a.value = '333333'
 </script>
 
 <template>
   <div>
-    vite yyds 承诺
+    vite yyds 承诺{{a}}
   </div>
 </template>
 
