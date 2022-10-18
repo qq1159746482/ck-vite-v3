c90bd4e0db8e6aa2ad887a02554a68a15d21a5b4
ck <1159746482@qq.com>
2022-10-18 10:48:09 +0800

    第六次更改文件提交

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,14 @@ import { Ref, ref } from 'vue';
 
 let a:Ref<string> = ref('')
 a.value = '333333'
+const list:number[] = [];
+list.push(1);
+list.push(Number(null))
 </script>
 
 <template>
   <div>
+    {{list}}
     vite yyds 承诺{{a}}
   </div>
 </template>
